dc6912c703ca08706b55bb47ff2208446477d452
Herbert Mühlburger <mail@muehlburger.at>
2020-02-13 10:52:03 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for _, d := range devices {
+		fmt.Println(d.Name)
+		for _, a := range d.Addresses {
+			fmt.Printf("  IP:      %s\n", a.IP)
+			fmt.Printf("  Netmask: %s\n", a.Netmask)
+		}
+	}
+}
